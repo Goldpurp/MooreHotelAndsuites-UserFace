ad5dc8bca9c3850ce232f68276ea9f023c7d3d61
Kehinde Razaq <Lilswatch112@gmail.com>
2026-02-10 20:37:41 +0100

    replaced logo image

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -62,7 +62,7 @@ const getDisplayName = () => {
           <Link to="/" onClick={handleLogoClick} className="flex items-center gap-[clamp(0.5rem,1vw,1rem)] group">
             <div className="logo-box relative">
               <div className="w-[clamp(2.7rem,3vw,3rem)] h-[clamp(2.7rem,3vw,3rem)] bg-[#dee2e6] rounded-sm flex items-center justify-center text-black font-black text-[clamp(1.1rem,1vw+0.8rem,1.6rem)] shadow-2xl shadow-primary/20 transition-all duration-500 animate-luxury-logo group-hover:scale-105">
-              <img src="/public/Logo/logo.png" alt="Moore Hotels & Suites" />
+              <img src="https://res.cloudinary.com/diovckpyb/image/upload/v1770752129/qkwdjjwzinvo3l6qa3jj.png" alt="Moore Hotels & Suites" />
               </div>
             </div>
 
